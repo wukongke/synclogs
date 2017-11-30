6518a2661ad3e05d98a3b76436a30f3177e261b9
wukongke <34113590+wukongke@users.noreply.github.com>
2017-11-30 11:40:25 +0800

    删除无用代码

--- src/app.js
+++ src/app.js
@@ -4,18 +4,12 @@ import './common/mgo';
 import './initDB';
 import mq from './common/mq';
 import task from './lib/task';
-// import sysVO from './models/systemparams';
 
 // aliyun心跳检测
 http.createServer((req, res) => {
   req.setTimeout(2 * 60 * 1000);
   res.end('');
 }).listen(process.env.PORT || 3000);
-
-// (async () => {
-//   const res = await sysVO.findOne();
-//   console.log(typeof res.lastSyncTime, res.lastSyncTime._bsontype, res.lastSyncTime.low_, res.lastSyncTime.high_);
-// })();
 
 /* eslint-disable */
 (async () => {
